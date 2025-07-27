140ef2aa8d306a75af400e007124f9e3f9684dbe
robiul <robiulstudies@gmail.com>
2025-07-27 23:27:36 +0600

    bug fix

--- routes/api.php
+++ routes/api.php
@@ -8,3 +8,7 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 Route::apiResource('posts', PostController::class);
+Route::get("list",[PostController::class,'index']);
+Route::post("list",[PostController::class,'store']);
+Route::put("list",[PostController::class,'index']);
+Route::delete("list",[PostController::class,'index']);
